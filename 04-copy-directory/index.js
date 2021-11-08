4cc98d1991c54560113e882b820a4081e905ead3
Sergey Mangilev <ssmangilev@gmail.com>
2021-11-08 23:25:23 +0100

    feat: solved fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,34 @@
+const fs = require('fs');
+const path = require('path');
 
+const dirName = 'files';
+const pathToOldFolder = path.join(__dirname, dirName);
+const pathToCopy = path.join(__dirname, `${dirName}-copy`);
+
+async function copyDir(pathToOldFolder, pathToCopy) {
+  await fs.readdir(pathToOldFolder, (err, files) => {
+    if (!err) {
+      files.forEach(async (file) => {
+        await fs.copyFile(`${pathToOldFolder}/${file}`, `${pathToCopy}/${file}`, () => {});
+      });
+    }
+  });
+}
+
+async function folderExists() {
+  await fs.stat(pathToCopy, async (err) => {
+    if (err) {
+      await fs.mkdir(pathToCopy, () => {});
+      await copyDir(pathToOldFolder, pathToCopy);
+    } else {
+      await fs.rmdir(pathToCopy, {recursive: true}, async () => {
+        await fs.mkdir(pathToCopy, () => {});
+        await copyDir(pathToOldFolder, pathToCopy);
+      }); 
+    } 
+  });
+  return;
+}
+
+
+folderExists();
