bb2a1e2134891fab74b1719f46f06fe8d6a05000
Sergey Mangilev <ssmangilev@gmail.com>
2021-11-09 17:34:11 +0100

    fix: added goodbye message

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -7,17 +7,22 @@ let rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-rl.question('Please enter a text here: ', () => {
+rl.question('Please enter a text here: ', (answer) => {
   let writeStream = fs.createWriteStream(path.join(__dirname, fileName), {
     encoding: 'utf8',
     flags: 'a',
   });
-  rl.on('line', (line) => {
+  if (answer && answer !== 'exit') {
+    writeStream.write(answer);
+  }
+  rl.on('line', async (line) => {
     if (line === 'exit') {
       rl.close();
       process.exit();
     } else {
-      writeStream.write(line);
+      await writeStream.write(line);
     }
   });
 });
+
+process.on('exit', () => console.log('bye'));
